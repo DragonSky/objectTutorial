215419a3118f245785b854408f92e54b56081bda
stewartpj <peterstewart67@optusnet.com.au>
2014-11-04 15:00:54 +1000

    Initial commit

---
+++ src/com/company/Point.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * Created by stewartpj on 4/11/2014.
+ */
+public class Point {
+    public int x = 0;
+    public int y = 0;
+    //constructor
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
